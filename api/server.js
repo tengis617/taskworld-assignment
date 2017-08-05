aae14165be00f96c53902cd88458add00efa7116
tengis617@gmail.com <tengis617@gmail.com>
2017-08-05 22:55:55 +0800

    name changes

---
+++ api/server.js
@@ -0,0 +1,25 @@
+import Hapi from 'hapi';
+import db from './mongodb';
+import routes from './routes';
+
+const server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: 8000,
+});
+server.state('session', {
+  ttl: 1000 * 60 * 60 * 24, // one day
+  encoding: 'base64json',
+  isSecure: false,
+});
+
+server.route(routes);
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.info('Server running at: ', server.info.uri);
+});
+
+export default server;
