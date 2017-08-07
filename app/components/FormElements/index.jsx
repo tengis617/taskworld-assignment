95a300df7334df5688d806f6b29041d14365ddd8
tengis617@gmail.com <tengis617@gmail.com>
2017-08-07 12:53:11 +0800

    id propery included, option now has 2 properties: value and text

--- app/components/FormElements/index.jsx
+++ app/components/FormElements/index.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import './FormElements.css';
 
 export function RadioButton({ onChange, name, options, checkedValue, icon }) {
-  const radioList = options.map(opt => (
-    <label className="radio-label">
+  const radioList = options.map((opt, i) => (
+    <label className="radio-label" key={name + opt.value} htmlFor={name + opt.value}>
       <input
+        id={name + opt.value}
         type="radio"
         name={name}
-        value={opt}
+        value={opt.value}
         onClick={onChange}
-        checked={checkedValue === opt}
-      /> {opt}
+        checked={checkedValue === opt.value}
+      /> {opt.text}
     </label>
   ));
   return (
@@ -23,12 +24,13 @@ export function RadioButton({ onChange, name, options, checkedValue, icon }) {
 
 export function DropdownList({ onChange, options, checkedValue, name }) {
 
-  const optionList = options.map((opt) => (
-    <option value={opt}>{opt}</option>
+  const optionList = options.map(opt => (
+    <option key={name + opt} value={opt}>{opt}</option>
   ));
   return (
     <select
       className="fancy-select"
+      id={name}
       name={name}
       value={checkedValue}
       onChange={onChange}
