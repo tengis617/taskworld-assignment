895c8117e67b2179e068309e22b47594c1171697
tengis617@gmail.com <tengis617@gmail.com>
2017-08-07 14:08:47 +0800

    default export to top

--- app/components/Preferences/index.jsx
+++ app/components/Preferences/index.jsx
@@ -3,6 +3,60 @@ import Section from '../Section';
 import Tile from '../Tile';
 import { DropdownList, RadioButton } from '../FormElements';
 
+
+export default function Preferences({ preferences, handleSubmit, handleChange }) {
+  return (
+    <Tile header="Edit Preferences">
+      <form onSubmit={handleSubmit}>
+        <Section header="Localization">
+          <LanguageSettings
+            selectedLanguage={preferences.language}
+            onChange={handleChange}
+          />
+          <TimeZoneSettings
+            onChange={handleChange}
+            selectedTimeZone={preferences.timeZone}
+          />
+          <CurrencySettings
+            onChange={handleChange}
+            selectedCurrency={preferences.currency}
+          />
+        </Section>
+        <Section header="Privacy">
+          <ProfileVisibilitySettings
+            onChange={handleChange}
+            checkedValue={preferences.profileVisibility}
+          />
+          <MessagesSettings
+            onChange={handleChange}
+            checkedValue={preferences.receiveMessagesFrom}
+          />
+          <RecentlyViewedSettings />
+        </Section>
+        <Section header="Content">
+          <CategoryListSettings
+            onChange={handleChange}
+            checkedValue={preferences.autoAddToCategoryList}
+          />
+        </Section>
+        <Section header="Notifications">
+          <NotificationSettings
+            onChange={handleChange}
+            checkedValue={preferences.smsNotifications}
+          />
+        </Section>
+        <Section>
+          <input
+            type="submit"
+            value="save preferences"
+            className="float-right fancy-button"
+          />
+        </Section>
+      </form>
+    </Tile>
+  );
+}
+
 export const CategoryListSettings = ({ onChange, checkedValue }) => (
   <div>
     <label>Category lists</label>
@@ -148,55 +202,4 @@ export const RecentlyViewedSettings = () => (
     <p><a href="">Delete all items</a></p>
   </div>
 );
-export default function Preferences({ preferences, handleSubmit, handleChange }) {
-  return (
-    <Tile header="Edit Preferences">
-      <form onSubmit={handleSubmit}>
-        <Section header="Localization">
-          <LanguageSettings
-            selectedLanguage={preferences.language}
-            onChange={handleChange}
-          />
-          <TimeZoneSettings
-            onChange={handleChange}
-            selectedTimeZone={preferences.timeZone}
-          />
-          <CurrencySettings
-            onChange={handleChange}
-            selectedCurrency={preferences.currency}
-          />
-        </Section>
-        <Section header="Privacy">
-          <ProfileVisibilitySettings
-            onChange={handleChange}
-            checkedValue={preferences.profileVisibility}
-          />
-          <MessagesSettings
-            onChange={handleChange}
-            checkedValue={preferences.receiveMessagesFrom}
-          />
-          <RecentlyViewedSettings />
-        </Section>
-        <Section header="Content">
-          <CategoryListSettings
-            onChange={handleChange}
-            checkedValue={preferences.autoAddToCategoryList}
-          />
-        </Section>
-        <Section header="Notifications">
-          <NotificationSettings
-            onChange={handleChange}
-            checkedValue={preferences.smsNotifications}
-          />
-        </Section>
-        <Section>
-          <input
-            type="submit"
-            value="save preferences"
-            className="float-right fancy-button"
-          />
-        </Section>
-      </form>
-    </Tile>
-  );
-}
+
