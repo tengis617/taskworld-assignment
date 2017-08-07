0fe93322e30dacbf77177de8f9c213f2ed66809c
tengis617@gmail.com <tengis617@gmail.com>
2017-08-07 12:51:58 +0800

    import statement chore

--- app/index.js
+++ app/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Settings from './pages/Settings';
 
-
+import 'milligram';
 import './normalize.css';
 import 'font-awesome/css/font-awesome.min.css';
-import 'milligram';
 import './global.css';
 
 
